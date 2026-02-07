287c8b9bf66c9ff7f82c8ecfb0e725fedda8102c
Mimosa01 <tarachoff5@gmail.com>
2026-02-07 18:31:32 +0900

    test map

--- src/shared/ui/molecules/Footer/Footer.tsx
+++ src/shared/ui/molecules/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FlagTriangleRight, Search } from "lucide-react";
+import { Search } from "lucide-react";
 import Button from "../../atoms/Button/Button";
 import StickyActionFooter from "../../atoms/StickyActonFooter/StickyActionFooter";
 
@@ -9,7 +9,6 @@ const Footer = () => {
       <Button className="shadow-md w-full aspect-2/1" variant="secondary">2</Button>
       <Button className="shadow-md w-full aspect-2/1" variant="secondary">3</Button>
       <Button className="shadow-md w-full aspect-2/1" variant="secondary"><Search /></Button>
-      <Button className="shadow-md w-full aspect-2/1" variant="secondary"><FlagTriangleRight className="text-rose-600"/></Button>
     </StickyActionFooter>
   )
 }
